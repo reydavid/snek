b7bb6154de7d4f141e94a895183984bdf4c52df0
Reydavid <5302552+reydavid@users.noreply.github.com>
2021-02-24 21:26:20 -0800

    index.js update for I don't remember...

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function drawGame(){
     if(score > 5){
         speed = 8;
     }
-    
+
     setTimeout(drawGame, 1000/ speed);
 }
 
@@ -156,23 +156,22 @@ function keyDown(event){
             return;
         yVelocity = -1;
         xVelocity = 0;
-        }
-        
-        //down
+    }
+    //down
     if(event.keyCode == 40){
         if(yVelocity == -1)
-            return;
+        return;
         yVelocity = 1;
         xVelocity = 0;
-        }
-        //left
+    }
+    //left
     if(event.keyCode == 37){
         if(xVelocity == 1)
-            return;
+        return;
         yVelocity = 0;
         xVelocity = -1;
-        }
-        //right
+    }
+    //right
     if(event.keyCode == 39){
         if(xVelocity == -1)
             return;
